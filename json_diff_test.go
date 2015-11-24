e8aa2acc54bff6a6b98b0e4beef9c8272a79558c
Kamila Hinckel <kamila.hinckel@neoway.com.br>
2015-11-24 13:19:12 -0200

    added fake information

--- json_diff_test.go
+++ json_diff_test.go
@@ -19,7 +19,7 @@ func TestShouldReturnFieldWhenValuesDiffer(t *testing.T) {
 	}
 
 	expected := []string{"str"}
-	actual := getJsonDiff(value, value2)
+	actual := GetJsonDiff(value, value2)
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Test failed. Expected", expected, "but returned", actual)
@@ -33,7 +33,7 @@ func TestShouldReturnEmptyWhenValuesMatch(t *testing.T) {
 	}
 
 	expected := []string{}
-	actual := getJsonDiff(value, value)
+	actual := GetJsonDiff(value, value)
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Test failed. Expected", expected, "but returned", actual)
@@ -45,7 +45,7 @@ func TestShouldReturnEmptyWhenValuesAreEmpty(t *testing.T) {
 	value2 := map[string]interface{}{}
 
 	expected := []string{}
-	actual := getJsonDiff(value, value2)
+	actual := GetJsonDiff(value, value2)
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Test failed. Expected", expected, "but returned", actual)
@@ -65,7 +65,7 @@ func TestShouldReturnFieldWhenTheNumberOfItensDiffer(t *testing.T) {
 	}
 
 	expected := []string{"bool"}
-	actual := getJsonDiff(value, value2)
+	actual := GetJsonDiff(value, value2)
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Test failed. Expected", expected, "but returned", actual)
@@ -84,7 +84,7 @@ func TestShoudReturnEmptyWhenValuesMatchAndAreInDifferentOrder(t *testing.T) {
 	}
 
 	expected := []string{}
-	actual := getJsonDiff(value, value2)
+	actual := GetJsonDiff(value, value2)
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Test failed. Expected", expected, "but returned", actual)
@@ -103,7 +103,7 @@ func TestShouldReturnFieldWhenThereIsAnArrayThatIsDifferent(t *testing.T) {
 	}
 
 	expected := []string{"str"}
-	actual := getJsonDiff(value, value2)
+	actual := GetJsonDiff(value, value2)
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Test failed. Expected", expected, "but returned", actual)
@@ -122,7 +122,7 @@ func TestShouldReturnFieldWhenTheElementsOfAnArrayDoesNotMatch(t *testing.T) {
 	}
 
 	expected := []string{"str"}
-	actual := getJsonDiff(value, value2)
+	actual := GetJsonDiff(value, value2)
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Test failed. Expected", expected, "but returned", actual)
@@ -131,23 +131,23 @@ func TestShouldReturnFieldWhenTheElementsOfAnArrayDoesNotMatch(t *testing.T) {
 
 func TestShouldReturnEmptyWhenTheElementsOfAnArrayAreJsonAndMatch(t *testing.T) {
 	byt := []byte(`{
-		"protocolo": "14921002",
-		"cnpj": "11002088000105",
-		"nomeEmpresarial": "BELLA INCORPORADORA E EMPREENDIMENTOS EPP LTDA",
+		"protocolo": "149123123",
+		"cnpj": "110020312312313",
+		"nomeEmpresarial": "INCORPORADORA E EMPREENDIMENTOS LTDA",
 		"naturezaJuridica": "SOCIEDADE ANONIMA FECHADA ",
-		"nire": "29200820065",
+		"nire": "2923123123",
 		"arquivamento": "25/07/2014",
 		"inicioAtividade": "1988-02-18",
-		"endereco": "R PATRICIO FARIAS",
+		"endereco": "RUA JOSE",
 		"numero": "",
-		"bairro": "ALPHAVILLE I",
+		"bairro": "ALPHAVILLE",
 		"complemento": "",
-		"municipio": "SALVADOR",
-		"cep": "41701015",
+		"municipio": "FAKE",
+		"cep": "123123123",
 		"uf": "SC",
-		"objetoSocial": "ALUGUEL DE MAQUINAS E EQUIPAMENTOS PARA ESCRITORIO, AGENCIAMENTO DE ESPACOS PARA PUBLICIDADE, EXCETO EM VEICULOS DE COMUNICACAO, ALUGUEL DE EQUIPAMENTOS CIENTIFICOS, MEDICOS E HOSPITALARES , SEM OPERADOR, COMERCIO VAREJISTA ESPECIALIZADO EM EQUIPAMENTOS E SUPRIMENTOS DE INFORMATICA, EDICAO DE CADASTROS, LISTAS E OUTROS PRODUTOS GRAFICOS, FOTOCOPIAS, IMPRESSAO DE MATERIAL PARA USO PUBLICITARIO, LOCACAO DE MAO-DE-OBRA TEMPORARIA, REPARACAO E MANUTENCAO DE COMPUTADORES E DE EQUIPAMENTOS PERIFERICOS, SERVICOS DE ACABAMENTOS GRAFICOS, EXCETO ENCADERNACAO E PLASTIFICACAO E SERVICOS DE MICROFILMAGEM.",
-		"capital": "20.000.000,00",
-		"capitalIntegralizado": "20.000.000,00",
+		"objetoSocial": "ABCD iABCDABCDABCDABCDABCDABCDABCDABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD.",
+		"capital": "10.000.000,00",
+		"capitalIntegralizado": "10.000.000,00",
 		"microEmpresa": "Nao",
 		"prazoDuracao": "Indeterminado",
 		"situacao": "REGISTRO ATIVO",
@@ -159,27 +159,27 @@ func TestShouldReturnEmptyWhenTheElementsOfAnArrayAreJsonAndMatch(t *testing.T) 
 		"moedaCapitalIntegralizado": "R$",
 		"socios": [
 		{
-				"nome": "MARIA ELKE GROSSENBACHER",
-				"documentoSocio":		"61382388934",
+				"nome": "MARIA EDUARDA",
+				"documentoSocio":		"123123123",
 				"valorParticipacao": "10.000.000,00",
 				"tipo": "Sócio Administrador",
 				"descricaoAdministrador": ""
 		},
 		{
-				"nome": "TATJANA GROSSENBACHER",
-				"documentoSocio":		"89118677991",
+				"nome": "TATIANA",
+				"documentoSocio":		"123123123",
 				"valorParticipacao": "10.000.000,00",
 				"tipo": "Sócio Administrador",
 				"descricaoAdministrador": ""
 		}
 		]
-}`)
+    }`)
 
 	var f interface{}
 	json.Unmarshal(byt, &f)
 
 	expected := []string{}
-	actual := getJsonDiff(f.(map[string]interface{}), f.(map[string]interface{}))
+	actual := GetJsonDiff(f.(map[string]interface{}), f.(map[string]interface{}))
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Test failed. Expected", expected, "but returned", actual)
@@ -188,23 +188,23 @@ func TestShouldReturnEmptyWhenTheElementsOfAnArrayAreJsonAndMatch(t *testing.T) 
 
 func TestShouldReturnFieldWhenTheElementsOfAnArrayAreJsonAndDoesNotMatch(t *testing.T) {
 	byt := []byte(`{
-		"protocolo": "14921002",
-		"cnpj": "11002088000105",
-		"nomeEmpresarial": "BELLA INCORPORADORA E EMPREENDIMENTOS EPP LTDA",
+		"protocolo": "149123123",
+		"cnpj": "110020312312313",
+		"nomeEmpresarial": "INCORPORADORA E EMPREENDIMENTOS LTDA",
 		"naturezaJuridica": "SOCIEDADE ANONIMA FECHADA ",
-		"nire": "29200820065",
+		"nire": "2923123123",
 		"arquivamento": "25/07/2014",
 		"inicioAtividade": "1988-02-18",
-		"endereco": "R PATRICIO FARIAS",
+		"endereco": "RUA JOSE",
 		"numero": "",
-		"bairro": "ALPHAVILLE I",
+		"bairro": "ALPHAVILLE",
 		"complemento": "",
-		"municipio": "SALVADOR",
-		"cep": "41701015",
+		"municipio": "FAKE",
+		"cep": "123123123",
 		"uf": "SC",
-		"objetoSocial": "ALUGUEL DE MAQUINAS E EQUIPAMENTOS PARA ESCRITORIO, AGENCIAMENTO DE ESPACOS PARA PUBLICIDADE, EXCETO EM VEICULOS DE COMUNICACAO, ALUGUEL DE EQUIPAMENTOS CIENTIFICOS, MEDICOS E HOSPITALARES , SEM OPERADOR, COMERCIO VAREJISTA ESPECIALIZADO EM EQUIPAMENTOS E SUPRIMENTOS DE INFORMATICA, EDICAO DE CADASTROS, LISTAS E OUTROS PRODUTOS GRAFICOS, FOTOCOPIAS, IMPRESSAO DE MATERIAL PARA USO PUBLICITARIO, LOCACAO DE MAO-DE-OBRA TEMPORARIA, REPARACAO E MANUTENCAO DE COMPUTADORES E DE EQUIPAMENTOS PERIFERICOS, SERVICOS DE ACABAMENTOS GRAFICOS, EXCETO ENCADERNACAO E PLASTIFICACAO E SERVICOS DE MICROFILMAGEM.",
-		"capital": "20.000.000,00",
-		"capitalIntegralizado": "20.000.000,00",
+		"objetoSocial": "ABCD iABCDABCDABCDABCDABCDABCDABCDABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD.",
+		"capital": "10.000.000,00",
+		"capitalIntegralizado": "10.000.000,00",
 		"microEmpresa": "Nao",
 		"prazoDuracao": "Indeterminado",
 		"situacao": "REGISTRO ATIVO",
@@ -216,40 +216,40 @@ func TestShouldReturnFieldWhenTheElementsOfAnArrayAreJsonAndDoesNotMatch(t *test
 		"moedaCapitalIntegralizado": "R$",
 		"socios": [
 		{
-				"nome": "MARIA ANTONIETA",   
-				"documentoSocio":		"61382388934",
+				"nome": "MARIA EDUARDA",
+				"documentoSocio":		"123123123",
 				"valorParticipacao": "10.000.000,00",
 				"tipo": "Sócio Administrador",
 				"descricaoAdministrador": ""
 		},
 		{
-				"nome": "TATJANA GROSSENBACHER",
-				"documentoSocio":		"89118677991",
+				"nome": "TATIANA",
+				"documentoSocio":		"123123123",
 				"valorParticipacao": "10.000.000,00",
 				"tipo": "Sócio Administrador",
 				"descricaoAdministrador": ""
 		}
 		]
-}`)
+    }`)
 
 	byt2 := []byte(`{
-		"protocolo": "14921002",
-		"cnpj": "11002088000105",
-		"nomeEmpresarial": "BELLA INCORPORADORA E EMPREENDIMENTOS EPP LTDA",
+		"protocolo": "149123123",
+		"cnpj": "110020312312313",
+		"nomeEmpresarial": "INCORPORADORA E EMPREENDIMENTOS LTDA",
 		"naturezaJuridica": "SOCIEDADE ANONIMA FECHADA ",
-		"nire": "29200820065",
+		"nire": "2923123123",
 		"arquivamento": "25/07/2014",
 		"inicioAtividade": "1988-02-18",
-		"endereco": "R PATRICIO FARIAS",
+		"endereco": "RUA JOSE",
 		"numero": "",
-		"bairro": "ALPHAVILLE I",
+		"bairro": "ALPHAVILLE",
 		"complemento": "",
-		"municipio": "SALVADOR",
-		"cep": "41701015",
+		"municipio": "FAKE",
+		"cep": "123123123",
 		"uf": "SC",
-		"objetoSocial": "ALUGUEL DE MAQUINAS E EQUIPAMENTOS PARA ESCRITORIO, AGENCIAMENTO DE ESPACOS PARA PUBLICIDADE, EXCETO EM VEICULOS DE COMUNICACAO, ALUGUEL DE EQUIPAMENTOS CIENTIFICOS, MEDICOS E HOSPITALARES , SEM OPERADOR, COMERCIO VAREJISTA ESPECIALIZADO EM EQUIPAMENTOS E SUPRIMENTOS DE INFORMATICA, EDICAO DE CADASTROS, LISTAS E OUTROS PRODUTOS GRAFICOS, FOTOCOPIAS, IMPRESSAO DE MATERIAL PARA USO PUBLICITARIO, LOCACAO DE MAO-DE-OBRA TEMPORARIA, REPARACAO E MANUTENCAO DE COMPUTADORES E DE EQUIPAMENTOS PERIFERICOS, SERVICOS DE ACABAMENTOS GRAFICOS, EXCETO ENCADERNACAO E PLASTIFICACAO E SERVICOS DE MICROFILMAGEM.",
-		"capital": "20.000.000,00",
-		"capitalIntegralizado": "20.000.000,00",
+		"objetoSocial": "ABCD iABCDABCDABCDABCDABCDABCDABCDABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD ABCD.",
+		"capital": "10.000.000,00",
+		"capitalIntegralizado": "10.000.000,00",
 		"microEmpresa": "Nao",
 		"prazoDuracao": "Indeterminado",
 		"situacao": "REGISTRO ATIVO",
@@ -261,21 +261,21 @@ func TestShouldReturnFieldWhenTheElementsOfAnArrayAreJsonAndDoesNotMatch(t *test
 		"moedaCapitalIntegralizado": "R$",
 		"socios": [
 		{
-				"nome": "MARIA ELKE GROSSENBACHER",
-				"documentoSocio":		"61382388934",
+				"nome": "MARIA",
+				"documentoSocio":		"123123123",
 				"valorParticipacao": "10.000.000,00",
 				"tipo": "Sócio Administrador",
 				"descricaoAdministrador": ""
 		},
 		{
-				"nome": "TATJANA GROSSENBACHER",
-				"documentoSocio":		"89118677991",
+				"nome": "TATIANA",
+				"documentoSocio":		"123123123",
 				"valorParticipacao": "10.000.000,00",
 				"tipo": "Sócio Administrador",
 				"descricaoAdministrador": ""
 		}
 		]
-}`)
+    }`)
 
 	var f interface{}
 	var g interface{}
@@ -283,7 +283,7 @@ func TestShouldReturnFieldWhenTheElementsOfAnArrayAreJsonAndDoesNotMatch(t *test
 	json.Unmarshal(byt2, &g)
 
 	expected := []string{"socios"}
-	actual := getJsonDiff(f.(map[string]interface{}), g.(map[string]interface{}))
+	actual := GetJsonDiff(f.(map[string]interface{}), g.(map[string]interface{}))
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Test failed. Expected", expected, "but returned", actual)
